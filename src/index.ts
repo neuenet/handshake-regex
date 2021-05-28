7e6a06517764cd9bd750d2c3171df3e54d9f3594
netop://ウエハ <paul@webb.page>
2021-05-28 14:09:39 -0700

    feat: Closes #2

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@
 
 ///  U T I L
 
-const _regex = "hs[a-z0-9]{40}";
+const _regex = "hs1[a-z02-9]{39,87}";
 
 
 
 ///  E X P O R T
 
 export const handshakeRegex = (suppliedString: string): boolean => {
-  if (String(suppliedString).match(new RegExp(_regex)))
+  if (String(suppliedString).match(new RegExp(`^${_regex}$`)))
     return true;
 
   return false;
--- test/index.ts
+++ test/index.ts
@@ -25,12 +25,24 @@ test("Returns false when invalid input is supplied", () => {
   assert.deepStrictEqual(dap("the distributed web will fail"), false);
 });
 
-test("Returns false when invalid wallet address  is supplied, using exported function", () => {
+test("Returns false when invalid wallet address is supplied", () => {
   assert.deepStrictEqual(dap("3MWvddmToBWnnPes3L45i1pRQDhPrLdznc"), false);
+});
+
+test("Returns false when an address with an extra 1 is supplied", () => {
+  assert.deepStrictEqual(dap("hs1q98ddwl2lcpnnzfv1vrqad80qu97w0q72cyq2uy3"), false);
 });
 
 test("Returns true when valid input is supplied", () => {
   assert.deepStrictEqual(dap("hs1q98ddwl2lcpnnzfvvrqad80qu97w0q72cyq2uy3"), true);
+});
+
+test("Returns true when short version 0 HNS address is supplied", () => {
+  assert.deepStrictEqual(dap("hs1qphmqky96jhyet3f5ch7d0q8q4yxzy0w0rz5ut3"), true);
+});
+
+test("Returns true when long version 0 HNS address is supplied", () => {
+  assert.deepStrictEqual(dap("hs1qk2g3zx3hr479nsq507nzgx5q3us60ewdp8jksj5hpkfumulqhy3qgsxwey"), true);
 });
 
 
@@ -40,12 +52,24 @@ test("Returns false when invalid input is supplied, using exported function", ()
   assert.deepStrictEqual(handshakeRegex("the distributed web will fail"), false);
 });
 
-test("Returns false when invalid wallet address  is supplied, using exported function", () => {
+test("Returns false when invalid wallet address is supplied, using exported function", () => {
   assert.deepStrictEqual(handshakeRegex("3MWvddmToBWnnPes3L45i1pRQDhPrLdznc"), false);
+});
+
+test("Returns false when an address with an extra 1 is supplied", () => {
+  assert.deepStrictEqual(handshakeRegex("hs1q98ddwl2lcpnnzfv1vrqad80qu97w0q72cyq2uy3"), false);
 });
 
 test("Returns true when valid input is supplied, using exported function", () => {
   assert.deepStrictEqual(handshakeRegex("hs1q98ddwl2lcpnnzfvvrqad80qu97w0q72cyq2uy3"), true);
+});
+
+test("Returns true when short version 0 HNS address is supplied", () => {
+  assert.deepStrictEqual(handshakeRegex("hs1qphmqky96jhyet3f5ch7d0q8q4yxzy0w0rz5ut3"), true);
+});
+
+test("Returns true when long version 0 HNS address is supplied", () => {
+  assert.deepStrictEqual(handshakeRegex("hs1qk2g3zx3hr479nsq507nzgx5q3us60ewdp8jksj5hpkfumulqhy3qgsxwey"), true);
 });
 
 
